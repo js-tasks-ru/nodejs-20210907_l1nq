2b2a159c688ab43de56c48bc82486bc9b20738d9
Artem Miroshnichenko <aimiroshnichenko@edu.hse.ru>
2021-09-07 13:38:05 +0300

    updated solution for 1-module 1-task

--- 01-intro/01-sum/sum.js
+++ 01-intro/01-sum/sum.js
@@ -1,5 +1,9 @@
 function sum(a, b) {
-  return a+b;
+  if (typeof a == 'number' && typeof b == 'number') {
+    return a + b;
+  } else {
+    throw new TypeError();
+  }
 }
 
 module.exports = sum;
