78c8fc39563945d3fa9a3a07e949504f9148b174
Artem Miroshnichenko <aimiroshnichenko@edu.hse.ru>
2021-09-15 23:23:45 +0300

    Событийный цикл - решено Стримы в Node.Js: Стрим с лимитом передачи данных - решено Файловый сервер - отдача файла - решено

--- 03-streams/01-limit-size-stream/LimitSizeStream.js
+++ 03-streams/01-limit-size-stream/LimitSizeStream.js
@@ -4,10 +4,17 @@ const LimitExceededError = require('./LimitExceededError');
 class LimitSizeStream extends stream.Transform {
   constructor(options) {
     super(options);
+    this.limit = options.limit;
+    this.sumChunk = 0;
   }
 
   _transform(chunk, encoding, callback) {
-
+    this.sumChunk += chunk.length;
+    if (this.sumChunk > this.limit) {
+      callback(new LimitExceededError);
+    } else {
+      callback(null, chunk);
+    }
   }
 }
 
