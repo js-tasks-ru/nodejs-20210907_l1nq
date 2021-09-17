0c08e21bf352214abdedac2eac9b549745125269
Artem Miroshnichenko <aimiroshnichenko@edu.hse.ru>
2021-09-17 15:14:43 +0300

    Не могу понять как отследить разрыв соединения, помогите, пожалуйста

--- 03-streams/04-file-server-post/server.js
+++ 03-streams/04-file-server-post/server.js
@@ -1,6 +1,7 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 
 const server = new http.Server();
 
@@ -12,7 +13,31 @@ server.on('request', (req, res) => {
 
   switch (req.method) {
     case 'POST':
-
+      if (!pathname.includes('/')) {
+        fs.access(filepath, (err) => {
+          if (err) {
+            let sumChunk = 0;
+            req.on('data', (chunk) => {
+              sumChunk += chunk.length;
+              if (sumChunk > 10000) {
+                res.statusCode = 413;
+                res.end('File more 1MB');
+              } else {
+                const streamWrite = fs.createWriteStream(filepath);
+                streamWrite.write(chunk);
+                res.statusCode = 201;
+                res.end('File created');
+              }
+            });
+          } else {
+            res.statusCode = 409;
+            res.end('File already exists');
+          }
+        });
+      } else {
+        res.statusCode = 400;
+        res.end('Не поддерживает вложенные вложенные файлы');
+      }
       break;
 
     default:
